679343f6546524eadc2ce9760e85a9f9b97af601
Dark <164911844+No0ne3696@users.noreply.github.com>
2025-11-25 15:25:57 +0400

    Ok

--- Unwalk+kurd.lua
+++ Unwalk+kurd.lua
@@ -1,2 +1,3 @@
+lua
 loadstring(game:HttpGet("https://raw.githubusercontent.com/No0ne3696/steal-a-brainrot/refs/heads/main/Unwalk"))()
 loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Kurd-hub-51808"))()
